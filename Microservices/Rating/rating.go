8c3d25f8d3b3ba2859a25800550ea4827a2d82a9
Kester <kesteryeo@hotmail.com>
2022-01-10 23:03:14 +0800

    started with get methods

--- Microservices/Rating/rating.go
+++ Microservices/Rating/rating.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
@@ -20,13 +21,31 @@ type Rating struct {
 	Anonymous         bool   `json:"Anonymous"`
 }
 
+var db *sql.DB
+
 func landing(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "~ Ratings & Comments Dashboard ~")
+}
+
+// View all ratings & comments
+func GetAllRatings(w http.ResponseWriter, r *http.Request) {
+	var ratings []Rating
+
 }
 
 func main() {
 	//start router
 	router := mux.NewRouter()
+
+	// establish db connection
+	// establish database connection
+	var err error
+	db, err = sql.Open("mysql", "root:password@tcp(db:3318)/rating_db")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+
 	// specify allowed headers, methods, & origins to allow CORS
 	headers := handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type"})
 	origins := handlers.AllowedOrigins([]string{"*"})
